ccc3878d0151399b4e4b13d7926f58e1b8ce519d
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-06-14 23:29:01 +0700

    change
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/tools/proxy/utils.go
+++ pkg/tools/proxy/utils.go
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !linux
-// +build !linux
+//go:build linux
+// +build linux
 
 package proxy
 
