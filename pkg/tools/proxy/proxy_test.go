dd4aa6ec3853b5c877a42a293375585641c6eddf
Nikita Skrynnik <nikita.skrynnik@xored.com>
2022-06-14 22:28:55 +0700

    change
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- pkg/tools/proxy/proxy_test.go
+++ pkg/tools/proxy/proxy_test.go
@@ -14,8 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build perm
-// +build perm
+//go:build linux
+// +build linux
 
 package proxy_test
 
